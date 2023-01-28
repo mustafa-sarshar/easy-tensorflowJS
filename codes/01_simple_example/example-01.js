3183dbbf206d8a642d4e106a0a50b4fe0f71aed2
musto <musti.sar4@gmail.com>
2023-01-28 22:27:50 +0100

    Add new examples

---
+++ codes/01_simple_example/example-01.js
@@ -0,0 +1,27 @@
+// const tf = require("@tensorflow/tfjs");
+import * as tf from "@tensorflow/tfjs";
+
+// Optional Load the binding:
+// Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+// require("@tensorflow/tfjs-node");
+
+// Train a simple model:
+const model = tf.sequential();
+model.add(
+  tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] }),
+  tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] }),
+  tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
+);
+model.add(tf.layers.dense({ units: 1, activation: "linear" }));
+model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+  epochs: 1000,
+  callbacks: {
+    onEpochEnd: (epoch, log) =>
+      console.log(`Epoch ${epoch}: loss = ${log.loss}`),
+  },
+});
